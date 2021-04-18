4c6eb2ff36e89cdd9ddd7d9973d6b0f08aeb0b35
Dylan Giesberts <dylan.giesberts@gmail.com>
2021-04-18 22:29:25 +0200

    Hopelijk laatste small screen fix

--- js/bevolking.js
+++ js/bevolking.js
@@ -6,7 +6,8 @@ let bevolkingmargin = {top: 20, right: 20, bottom: 30, left: 50},
     bevolkingheight = 500 - bevolkingmargin.top - bevolkingmargin.bottom;
 
 if (bevolking.clientWidth < 500) {
-    bevolkingwidth = window.innerWidth-200;
+    console.log(window.innerWidth)
+    bevolkingwidth = window.innerWidth-100;
 }
 
 const color = ["hsl(120, 73.4%, 74.9%)", "hsl(120, 60.8%, 50%)",
